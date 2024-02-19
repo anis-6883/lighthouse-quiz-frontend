c7e12876dfe59cdb791872c6bc97d7fe26cde8c4
Al Sajid <alsajiduzzaman@outlook.com>
2024-02-19 12:06:10 +0600

    some update in project structure

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import("prettier").Config} */
 const config = {
-  // printWith: 120,
+  printWith: 150,
   singleQuote: true,
-  // semi:false,
+  semi: false,
   trailingComma: 'es5',
   tabWidth: 2,
-  useTabs: false,
-  plugins: ['prettier-plugin-tailwindcss'],
+  useTabs: true,
+  plugins: ['prettier-plugin-tailwindcss']
 };
 export default config;
--- src/app/(frontend)/(main-layout)/settings/help-center/page.tsx
+++ src/app/(frontend)/(main-layout)/settings/help-center/page.tsx
@@ -3,21 +3,30 @@ import { metaObject } from '@/config/site.config';
 import { FaArrowLeft, FaRegBell } from 'react-icons/fa6';
 import HelpCenterTab from '../../components/HelpCenterTab';
 import TopNavigation from '../../components/TopNavigation';
+import getData from '@/utils/fetch/getData';
 export const metadata = {
-  ...metaObject('Settings - Help Center'),
+  ...metaObject('Settings - Help Center')
 };
 
-export default function Page() {
+export type FAQ = {
+  title: string;
+  description: string;
+};
+
+export default async function Page() {
+  const faq: FAQ = await getData('faq');
+
   return (
     <>
-      <div className="w-full pb-5 pt-4">
+      <div className='w-full pb-5 pt-4'>
         <TopNavigation
-          icon={<FaArrowLeft className="text-2xl" />}
-          pageName="help center"
-          notification={<FaRegBell className="text-3xl font-medium" />}
+          icon={<FaArrowLeft className='text-2xl' />}
+          pageName='help center'
+          notification={<FaRegBell className='text-3xl font-medium' />}
         />
       </div>
-      <HelpCenterTab />
+
+      <HelpCenterTab faq={faq} />
     </>
   );
 }
