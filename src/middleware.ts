d5bd2cb14c5a1611120a8344ae921cd8710011ab
Al Sajid <alsajiduzzaman@outlook.com>
2024-03-11 10:41:58 +0600

    root route added in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,10 +8,10 @@ export default withAuth(
 
     if (token && token.accessToken && token.role) {
       if (token.role === 'admin') {
-        if (pathname.startsWith('/auth') || pathname.startsWith('/app') || pathname === '/admin/login')
+        if (pathname.startsWith('/auth') || pathname.startsWith('/app') || pathname === '/admin/login' || pathname === '/')
           return NextResponse.redirect(new URL('/admin/dashboard', req.url))
       } else if (token.role === 'user') {
-        if (pathname.startsWith('/admin') || pathname.startsWith('/auth')) return NextResponse.redirect(new URL('/app', req.url))
+        if (pathname.startsWith('/admin') || pathname.startsWith('/auth') || pathname === '/') return NextResponse.redirect(new URL('/app', req.url))
       }
     } else {
       if (pathname.startsWith('/admin') && pathname !== '/admin/login') return NextResponse.redirect(new URL('/admin/login', req.url))
@@ -26,5 +26,5 @@ export default withAuth(
 )
 
 export const config = {
-  matcher: ['/app/:path*', '/auth/:path*', '/admin/:path*'],
+  matcher: ['/app/:path*', '/auth/:path*', '/admin/:path*', '/'],
 }
